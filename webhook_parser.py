26a2bef8803b0387fe8298cae55416d708f5818d
Oleksandr Diamantopulo <odiam@if504.softservecom.com>
2019-03-26 11:50:21 +0200

    executed code

--- webhook_parser.py
+++ webhook_parser.py
@@ -1,84 +1,62 @@
 #!/usr/bin/env python
 
 '''
-USAGE: webhook-parser.py -a/--authtoken= <authorization token> -p <pattern in the filepath>  <webhook payload file>)
-   or  webhook-parser.py -f/--filewithtoken= <file, that contains token> -p <pattern in the filepath> <webhook payload file>')
+USAGE: webhook-parser.py  -p <pattern in the filepath>  <webhook payload file>)
+
 DEFAULT VALUES:
-    filewithtoken = '/home/<username>/.ssh/github-token'
     pattern       = 'puppet'
 '''
 import json
 import os,sys, getopt
 import subprocess
 
-# get username for token  path   !!!!!!!!!1
-# This script has to receive github 'push' webhook payload in json format
-# 
 
-def cliExec(command, arg):
+def parse_payload(data, pattern):
 
-    process = subprocess.Popen([command, arg], stdout=subprocess.PIPE)
-    output, error = process.communicate()
-    return output, error
-
-def main(argv):
-
-    authToken    = ''
-    pattern      = 'puppet'
-    tokenPath    = '/home/odiam/.ssh/github-token'
-    tempTreeFile = "/tmp/git-tree-recursive.json"
-
-    try:
-        opts, args = getopt.getopt(argv,"a:f:p:",["authtoken=","filewithtoken="])
-    except getopt.GetoptError:
-        print('getopt error. Usage: ' + sys.argv[0] + ' -a/--authtoken= <authorization token>  <webhook payload file>')
-        print(' or ' + sys.argv[0] + ' -f/--filewithtoken= <file, that contains token> <webhook payload file>')
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt in ("-a", "--authtoken"):
-            authToken = arg
-        elif opt in ("-f", "--filewithtoken"):
-            tokenPath = arg
-        elif opt in ("-p", "--pattern"):
-            pattern = arg
-        else:
-            print('Usage: ' + sys.argv[0] + ' -a/--authtoken= <authorization token>  <webhook payload file>')
-            sys.exit()
-
-    with  open(args[0]) as payload:
+    with  open(data) as payload:
         jsonData = json.load(payload)
     head_sha = jsonData.get("after")
     repo     = jsonData.get("repository")
     commits  = jsonData.get("commits")
 
-    commitList = []
+    addModList = []
+    removedList =[]
     for item in range(len(commits)):
         itemID = commits[item].get("id")
-        commitList.append(itemID)
-    
-    trees_url_prefix = str.split(str(repo.get("trees_url")),'{')[0]
-    authToken = cliExec('cat', tokenPath)[0].replace("\n","")
-    
-    curlArg = trees_url_prefix + '/'+ head_sha + '?recursive=1 -H "Authorization: token ' + authToken +'" -o '+ tempTreeFile
-    os.system("curl "+curlArg)
-    # LoadedTree = json.loads(os.system("curl "+curlArg))
-    
-    with open(tempTreeFile) as TreeFile:
-        # if not truncated
-        LoadedTree = json.load(TreeFile)
-    # os.remove(tempTreeFile)
+        if commits[item].get("removed"):
+            removedList.append(commits[item].get("removed"))
+        else:
+            if commits[item].get("added"):
+                addModList.append(commits[item].get("added"))
+            else:
+                addModList.append(commits[item].get("modified"))
 
     pathList = []
-    treeList   = LoadedTree.get("tree")
+    for item in range(len(addModList)):
+        for itemPath in range(len(addModList[item])):
+            iteration = addModList[item][itemPath]
+            if (pattern in iteration):
+                pathList.append(iteration)
+    
+    return pathList
 
-    for item in range(len(treeList)):
-        itemPath = treeList[item].get("path")
-        itemSHA  = treeList[item].get("sha")
-        if (itemSHA in commitList) and (pattern in itemPath):
-            pathList.append(itemPath)
-    
-    print(" ".join(pathList))
-    
+def main(argv):
+
+    pattern      = 'puppet'
+
+    try:
+        opts, args = getopt.getopt(argv,"p:",["pattern="])
+    except getopt.GetoptError:
+        print('getopt error. Usage: ' + sys.argv[0] + ' -p <pattern  <webhook payload file>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-p", "--pattern"):
+            pattern = arg
+        else:
+            print('Usage: ' + sys.argv[0] + ' -p <pattern  <webhook payload file>')
+            sys.exit()
+
+    print(" ".join(parse_payload(args[0], pattern)))
 
 if __name__ == "__main__":
     main(sys.argv[1:])
